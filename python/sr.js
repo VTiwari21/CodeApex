c3b9bf491521742bc26332b35e5a700357afa3c6
V.Tiwari <21tiwarivaibhav@gmail.com>
2026-04-05 15:59:04 +0530

    Add files via upload

---
+++ python/sr.js
@@ -0,0 +1,24 @@
+ function s() {
+            window.open( "../index.html", "_blank");
+        }
+ function tab() {
+            window.open("comp.html", "_blank");
+        }
+ function tabC() {
+            window.open("c.html", "_blank");
+        }
+ function tabCP() {
+            window.open("cpp.html", "_blank");
+        }
+ function tabCS() {
+            window.open("csharp.html", "_blank");
+        }
+ function tabJ() {
+            window.open("java.html", "_blank");
+        }
+ function tabP() {
+            window.open("python.html", "_blank");
+        }
+function f(){
+    window.open('https://docs.google.com/forms/d/e/1FAIpQLSdio_wGJwr4KrbnapqgGsxZKygM_-AdX_m4x9DalzSsm86R1w/viewform?usp=header',"_blank");
+}
